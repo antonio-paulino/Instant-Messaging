76c94a02f433384f1e408d960b910dd6c27db67c
António Paulino <antonioppaulino2@gmail.com>
2024-10-06 14:32:27 +0100

    Add im-http-api initial files

---
+++ code/jvm/im-http-api/src/main/kotlin/App.kt
@@ -0,0 +1,12 @@
+package pt.isel.daw.daw_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+	runApplication<App>(*args)
+}
+
